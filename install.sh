a2b411a59126d705ce544aa0969b5d958a72394b
hamburgerghini1 <98650216+hamburgerghini1@users.noreply.github.com>
2023-08-17 23:02:27 +0300

    Chsnged apt to pacman

--- install.sh
+++ install.sh
@@ -1,68 +1,63 @@
 #!/bin/bash
 
-# going to home folder
+# Going to home folder
 cd
 
-# instaliing requirements
+# Instaliing requirements
 # i3
-echo -e "\n\e[40mInstalling i3 window manager...\n"
-sudo apt install i3
+echo -e "\nInstalling i3 window manager...\n"
+sudo pacman -S i3
 
 # polybar
-echo -e "\n\e[40mInstalling polybar...\n"
-sudo apt-get install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2
-git clone https://github.com/jaagr/polybar.git
-cd polybar && ./build.sh
-sudo apt install polybar
+echo -e "\nInstalling polybar...\n"
+sudo pacman -S cmake cairo xcb-util-cursor xcb-util-image xcb-util-wm xcb-util-xrm pkg-config python xcb-proto libmpdclient libnl libcurl-compat
+git clone https://github.com/polybar/polybar.git
+cd polybar && mkdir build && cd build && cmake ..
+make -j$(nproc)
+sudo make install
 cd
+mkdir -p $HOME/.config/polybar
 install -Dm644 /usr/share/doc/polybar/config $HOME/.config/polybar/config
 
 # mate-terminal
-echo -e "\n\e[40mInstalling mate-terminal...\n"
-sudo apt install mate-terminal
+echo -e "\nInstalling mate-terminal...\n"
+sudo pacman -S mate-terminal
 
 # compton
-echo -e "\n\e[40mInstalling compton...\n"
-sudo apt install compton
+echo -e "\nInstalling compton...\n"
+sudo pacman -S picom
 
 # feh
-echo -e "\n\e[40mInstalling feh...\n"
-sudo apt install feh
-sudo apt install imagemagick
-sudo apt install ffmpeg
+echo -e "\nInstalling feh...\n"
+sudo pacman -S feh imagemagick ffmpeg
 
 # neofetch
-echo -e "\n\e[40mInstalling neofetch...\n"
-sudo apt install neofetch
+echo -e "\nInstalling neofetch...\n"
+sudo pacman -S neofetch
 
 # brave-browser
-echo -e "\n\e[40mInstalling Dependencies for brave-browser...\n"
-sudo apt install apt-transport-https curl
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-echo -e "\n\e[40mUpdating packages...\n"
-sudo apt update
-echo -e "\n\e[40mInstalling brave-browser...\n"
-sudo apt install brave-browser
+echo -e "\nInstalling Dependencies for brave-browser...\n"
+sudo pacman -S curl
+yay -S brave-bin
 
 # w3m
-echo -e "\n\e[40mInstalling w3m...\n"
-sudo apt install w3m
+echo -e "\nInstalling w3m...\n"
+sudo pacman -S w3m
 
 # i3-gaps
-echo -e "\n\e[40mInstalling i3-gaps...\n"
-sudo apt install i3-gaps
+echo -e "\nInstalling i3-gaps...\n"
+yay -S i3-gaps
 
 # ranger
-echo -e "\n\e[40mInstalling ranger...\n"
-sudo apt install ranger
+echo -e "\nInstalling ranger...\n"
+sudo pacman -S ranger
 
 # rofi
-echo -e "\n\e[40mInstalling rofi...\n"
-sudo apt install rofi
+echo -e "\nInstalling rofi...\n"
+sudo pacman -S rofi
 
 # wallset
-echo -e "\n\e[40mInstalling wallset...\n"
+echo -e "\nInstalling wallset...\n"
 git clone https://github.com/terroo/wallset
 cd wallset
 sudo bash install.sh
@@ -70,7 +65,7 @@ sudo ./install.sh --force #force installing
 cd
 
 # polybar-themes
-echo -e "\n\e[40mInstalling polybar-themes...\n"
+echo -e "\nInstalling polybar-themes...\n"
 git clone --depth=1 https://github.com/adi1090x/polybar-themes.git
 cd polybar-themes
 chmod +x setup.sh
@@ -81,12 +76,12 @@ sleep 7
 cd
 
 # zsh
-echo -e "\n\e[40mInstalling zsh...\n"
-sudo apt install zsh
+echo -e "\nInstalling zsh...\n"
+sudo pacman -S zsh
 
 # oh-my-zsh
-echo -e "\n\e[40mInstalling oh-my-zsh...\n"
+echo -e "\nInstalling oh-my-zsh...\n"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 chsh -s $(which zsh)
